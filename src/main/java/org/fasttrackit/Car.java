fb5d30e11ebcc24571c57aa80029696a38e292d1
Alex <andreica.alexandru2000@gmail.com>
2019-11-13 21:17:45 +0200

    studied classes and variables

---
+++ src/main/java/org/fasttrackit/Car.java
@@ -0,0 +1,15 @@
+package org.fasttrackit;
+
+public class Car {
+
+//    instance varables
+    String name;
+    String color;
+    double mileage;
+    double maxSpeed;
+    double fuelLevel;
+    double travelDistance;
+    boolean running;
+    int doorCount;
+
+}
